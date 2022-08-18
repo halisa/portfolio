ef78695f562295f8ef72ae9cc9407c7f68a38172
halisa <lh.halisa@gmail.com>
2022-08-18 00:37:30 -0700

    Update

---
+++ src/App.tsx
@@ -0,0 +1,17 @@
+import './style/style.css';
+import { BrowserRouter } from "react-router-dom";
+import HomePage from './pages/HomePage';
+import React from 'react';
+
+function App() {
+  return (
+    // <BrowserRouter>
+    //   <Routes>
+    //     <Route path="/" element={<HomePage />} />
+    //   </Routes>
+    // <BrowserRouter/>
+    <h1 id="root">hi</h1>
+  );
+}
+
+export default App;
