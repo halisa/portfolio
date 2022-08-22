f643b9b5ae1e924b755bdedb62f170f9cfd437ed
halisa <lh.halisa@gmail.com>
2022-08-21 20:53:30 -0700

    update

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,25 @@
-import './style/style.css';
-import { BrowserRouter } from "react-router-dom";
-import HomePage from './pages/HomePage';
 import React from 'react';
+import logo from './logo.svg';
+import './App.css';
 
 function App() {
   return (
-    // <BrowserRouter>
-    //   <Routes>
-    //     <Route path="/" element={<HomePage />} />
-    //   </Routes>
-    // <BrowserRouter/>
-    <h1 id="root">hi</h1>
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.tsx</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
   );
 }
 
