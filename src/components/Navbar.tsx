0415f6ac0b206f1a2f6fc7a0a93299799b573c08
Lisa Ha <48501960+halisa@users.noreply.github.com>
2023-07-03 19:17:30 -0700

    update navbar

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,14 +1,27 @@
 import React from "react";
 import { Link } from "react-router-dom";
 import Logo from "../assets/Logo";
-declare module "*.svg"
+import logo from "../assets/logonav.png"
+// declare module "*.svg"
 
 const Navbar = () => {
     return(
         <div className="navbar">
             <div className="navbar-container">
+                <div className="filter-elements">
+                    <div className="all">
+                        <div className="filter-text">all</div>
+                    </div>
+                    <div className="design">
+                        <div className="filter-text">design</div>
+                    </div>
+                    <div className="illustration">
+                        <div className="filter-text">illustration</div>
+                    </div>
+                </div>
                 <div className="logo">
-                        <Link to="/"><Logo /></Link>
+                        {/* <Link to="/"><Logo /></Link> */}
+                        <img src={logo} alt="" className="logo-nav"/>
 
                 </div>
                 <div className="nav-elements">
