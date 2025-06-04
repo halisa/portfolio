9168ffb7cd88bd3a5646bafd893e73712a2486af
Lisa Ha <48501960+halisa@users.noreply.github.com>
2025-06-03 19:40:10 -0700

    update

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,4 +1,22 @@
-function toggleColorMode() {
-  var element = document.body;
-  element.classList.toggle("dark-mode");
+const links = [
+    "https://halisa.github.io/dvp.html",
+    "https://halisa.github.io/marcom.html",
+    "https://halisa.github.io/games.html"
+    // "https://halisa.github.io/",
+    // "https://halisa.github.io/",
+    // "https://halisa.github.io/",
+    // "https://halisa.github.io/",
+    // "https://halisa.github.io/",
+    // "https://halisa.github.io/",
+    // "https://halisa.github.io/"
+]
+
+function getRandomLink() {
+    const randomIndex = Math.floor(Math.random() * links.length);
+    return links[randomIndex];
+}
+
+function goToRandomLink() {
+  const randomLink = getRandomLink();
+  window.location.href = randomLink;
 }
