61c140b2b4317df88b62a20604fb77b0cb91571a
Lisa Ha <48501960+halisa@users.noreply.github.com>
2025-06-03 21:52:00 -0700

    update footer

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,14 +1,17 @@
 const links = [
+    "https://halisa.github.io/apisa.html",
+    "https://halisa.github.io/archive.html",
+    "https://halisa.github.io/chiu.html",
+    "https://halisa.github.io/city.html",
     "https://halisa.github.io/dvp.html",
+    "https://halisa.github.io/games.html",
+    "https://halisa.github.io/hilltop.html",
+    "https://halisa.github.io/illustration.html",
     "https://halisa.github.io/marcom.html",
-    "https://halisa.github.io/games.html"
-    // "https://halisa.github.io/",
-    // "https://halisa.github.io/",
-    // "https://halisa.github.io/",
-    // "https://halisa.github.io/",
-    // "https://halisa.github.io/",
-    // "https://halisa.github.io/",
-    // "https://halisa.github.io/"
+    "https://halisa.github.io/moon.html",
+    "https://halisa.github.io/omg.html",
+    "https://halisa.github.io/seatac.html",
+    "https://halisa.github.io/slow.html"
 ]
 
 function getRandomLink() {
